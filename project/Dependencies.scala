4612873588b537b0950bcbbca3d351fa8a19183a
Scala Steward <scala_steward@virtuslab.com>
2024-01-30 20:34:32 +0000

    ⬆️ Update tapir-http4s-client from 1.9.7 to 1.9.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
         "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % "1.9.7",
         "com.softwaremill.sttp.tapir" %% "tapir-opentelemetry-metrics" % "1.9.7",
         "com.softwaremill.sttp.tapir" %% "tapir-iron" % "1.9.7",
-        "com.softwaremill.sttp.tapir" %% "tapir-http4s-client" % "1.9.7" % Test,
+        "com.softwaremill.sttp.tapir" %% "tapir-http4s-client" % "1.9.8" % Test,
         "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % "1.9.7" % Test,
         "com.softwaremill.sttp.client3" %% "core" % "3.9.2" % Test
         //    "com.softwaremill.sttp.tapir"   %% "tapir-swagger-ui-bundle" % "1.9.1",
