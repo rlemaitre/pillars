4920820fdddff803f21b2ccc485d0c999674b6a1
Scala Steward <scala_steward@virtuslab.com>
2024-03-08 23:06:11 +0000

    ⬆️ Update opentelemetry-exporter-otlp from 1.35.0 to 1.36.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
 
     val observability: Seq[ModuleID] = Seq(
       "org.typelevel"   %% "otel4s-java"                               % "0.4.0",
-      "io.opentelemetry" % "opentelemetry-exporter-otlp"               % "1.35.0" % Runtime,
+      "io.opentelemetry" % "opentelemetry-exporter-otlp"               % "1.36.0" % Runtime,
       "io.opentelemetry" % "opentelemetry-sdk-extension-autoconfigure" % "1.35.0" % Runtime
     )
 
