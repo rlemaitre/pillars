a6a482f17696b1e4965d69c0e7e905275d3482bd
Raphaël Lemaitre <rlemaitre@users.noreply.github.com>
2024-03-13 15:13:58 +0100

    feat: Add OpenAPI generation (#81)

---
+++ modules/core/src/main/scala/pillars/commands.scala
@@ -0,0 +1,9 @@
+package pillars
+
+import com.monovore.decline.Command
+
+val openApiGenerator = Command(
+  name = "openapi-generator",
+  header = "Generates a client from an OpenAPI specification"
+):
+    ???
