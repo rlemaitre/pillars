4950b066a9cf62aca711c31138ef7709d01b33da
Scala Steward <scala_steward@virtuslab.com>
2024-04-01 19:37:16 +0000

    chore: Update scribe-file from 3.13.0 to 3.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
       "com.outr" %% "scribe-cats"       % "3.13.2",
       "com.outr" %% "scribe-slf4j"      % "3.13.0",
       "com.outr" %% "scribe-json-circe" % "3.13.0",
-      "com.outr" %% "scribe-file"       % "3.13.0"
+      "com.outr" %% "scribe-file"       % "3.13.2"
     )
 
     val tests: Seq[ModuleID] = Seq(
