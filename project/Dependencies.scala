82f08273501dd0cc5ac19ba414878963fcb2c160
Scala Steward <scala_steward@virtuslab.com>
2024-04-17 18:08:50 +0000

    chore: Update flyway-database-postgresql from 10.10.0 to 10.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -98,7 +98,7 @@ object Dependencies {
 
     val migrationsRuntime: Seq[ModuleID] = Seq(
       "org.postgresql" % "postgresql"                 % "42.7.3",
-      "org.flywaydb"   % "flyway-database-postgresql" % "10.10.0"
+      "org.flywaydb"   % "flyway-database-postgresql" % "10.11.0"
     )
     val migrations: Seq[ModuleID]        = Seq(
       "org.flywaydb" % "flyway-core" % "10.11.0"
