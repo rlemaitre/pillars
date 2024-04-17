47b8cedc4b76583e2c831751e4ccfa11388acf51
Scala Steward <scala_steward@virtuslab.com>
2024-04-17 18:08:34 +0000

    chore: Update tapir-sttp-stub-server from 1.10.3 to 1.10.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
       "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs"          % "1.10.4",
       "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml"          % "0.8.0",
       "com.softwaremill.sttp.tapir"   %% "tapir-http4s-client"         % "1.10.4" % Test,
-      "com.softwaremill.sttp.tapir"   %% "tapir-sttp-stub-server"      % "1.10.3" % Test,
+      "com.softwaremill.sttp.tapir"   %% "tapir-sttp-stub-server"      % "1.10.4" % Test,
       "com.softwaremill.sttp.client3" %% "core"                        % "3.9.5"  % Test
       //    "com.softwaremill.sttp.tapir"   %% "tapir-swagger-ui-bundle" % "1.9.1",
     )
