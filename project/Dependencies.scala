25b463a4c3ac15352f9779c8d6bfd880a1f2acf5
Scala Steward <scala_steward@virtuslab.com>
2024-05-27 17:11:29 +0000

    chore: Update tapir-opentelemetry-metrics from 1.10.5 to 1.10.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
     private val tapir = Seq(
       "com.softwaremill.sttp.tapir"   %% "tapir-http4s-server"         % "1.10.7",
       "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"            % "1.10.7",
-      "com.softwaremill.sttp.tapir"   %% "tapir-opentelemetry-metrics" % "1.10.5",
+      "com.softwaremill.sttp.tapir"   %% "tapir-opentelemetry-metrics" % "1.10.7",
       "com.softwaremill.sttp.tapir"   %% "tapir-iron"                  % "1.10.7",
       "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs"          % "1.10.7",
       "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml"          % "0.9.0",
