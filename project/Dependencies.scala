cbae764e728171a611245f5d6e1420e2f9c7e752
Scala Steward <scala_steward@virtuslab.com>
2024-06-08 17:18:20 +0000

    chore: Update cats-core from 2.10.0 to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 object Dependencies {
     val effect: Seq[ModuleID] = Seq(
-      "org.typelevel" %% "cats-core"             % "2.10.0",
+      "org.typelevel" %% "cats-core"             % "2.12.0",
       "org.typelevel" %% "cats-effect"           % "3.5.4",
       "co.fs2"        %% "fs2-core"              % "3.10.2",
       "org.typelevel" %% "cats-collections-core" % "0.9.8",
