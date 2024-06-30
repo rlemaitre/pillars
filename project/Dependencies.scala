680c5ecac9d03c48e2a1fdedd4898f70bf0036bd
Scala Steward <scala_steward@virtuslab.com>
2024-06-30 15:58:13 +0000

    chore: Update http4s-netty-client from 0.5.16 to 0.5.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
     )
 
     val http4sClient: Seq[ModuleID] = Seq(
-      "org.http4s" %% "http4s-netty-client" % "0.5.16"
+      "org.http4s" %% "http4s-netty-client" % "0.5.17"
     )
     val http4sServer: Seq[ModuleID] = Seq(
       "org.http4s" %% "http4s-netty-server" % "0.5.16"
