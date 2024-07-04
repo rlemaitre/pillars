19dc7a3e7d3dcd5c31a34af8cc7bf592f1f7358b
Scala Steward <scala_steward@virtuslab.com>
2024-07-04 19:52:09 +0000

    chore: Update flyway-core from 10.14.0 to 10.15.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -101,7 +101,7 @@ object Dependencies {
       "org.flywaydb"   % "flyway-database-postgresql" % "10.14.0"
     )
     val migrations: Seq[ModuleID]        = Seq(
-      "org.flywaydb" % "flyway-core" % "10.14.0"
+      "org.flywaydb" % "flyway-core" % "10.15.2"
     ) ++ tests ++ testContainers ++ migrationsRuntime.map(_ % Test)
 
     val fs2Rabbit: Seq[ModuleID] = Seq(
