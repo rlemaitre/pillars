c3d271cf5085488735db8b79ceaabe7a89c7ee1d
Raphaël Lemaitre <rlemaitre@users.noreply.github.com>
2024-07-25 15:39:46 +0200

    feat(core): #129 Include OpenAPI documentation on HTTP servers (#130)

--- modules/core/src/main/scala/pillars/ApiServer.scala
+++ modules/core/src/main/scala/pillars/ApiServer.scala
@@ -5,6 +5,7 @@ import cats.effect.Resource.ExitCase
 import cats.syntax.all.*
 import com.comcast.ip4s.*
 import io.circe.Codec
+import io.circe.derivation.Configuration
 import io.github.iltotore.iron.*
 import pillars.Controller.HttpEndpoint
 import pillars.PillarsError.Code
@@ -15,14 +16,19 @@ trait ApiServer[F[_]]:
     def start(endpoints: List[HttpEndpoint[F]]): F[Unit]
 
 object ApiServer:
-    def apply[F[_]]: Run[F, ApiServer[F]]                                                                   = summon[Pillars[F]].apiServer
-    def init[F[_]: Async](config: Config, observability: Observability[F], logger: Scribe[F]): ApiServer[F] =
+    def apply[F[_]]: Run[F, ApiServer[F]] = summon[Pillars[F]].apiServer
+    def init[F[_]: Async](
+        config: Config,
+        infos: AppInfo,
+        observability: Observability[F],
+        logger: Scribe[F]
+    ): ApiServer[F] =
         (endpoints: List[HttpEndpoint[F]]) =>
             Async[F].whenA(config.enabled):
                 for
                     _ <- logger.info(s"Starting API server on ${config.http.host}:${config.http.port}")
                     _ <- HttpServer
-                             .build("api", config.http, observability, endpoints)
+                             .build("api", config.http, config.openApi, infos, observability, endpoints)
                              .onFinalizeCase:
                                  case ExitCase.Errored(e) => logger.error(s"API server stopped with error: $e")
                                  case _                   => logger.info("API server stopped")
@@ -33,7 +39,14 @@ object ApiServer:
         final override def code: Code = Code("API")
     end Error
 
-    final case class Config(enabled: Boolean, http: HttpServer.Config = defaultHttp) derives Codec.AsObject
+    final case class Config(
+        enabled: Boolean,
+        http: HttpServer.Config = defaultHttp,
+        openApi: HttpServer.Config.OpenAPI = HttpServer.Config.OpenAPI()
+    )
+
+    given Configuration = Configuration.default.withKebabCaseMemberNames.withKebabCaseConstructorNames.withDefaults
+    given Codec[Config] = Codec.AsObject.derivedConfigured
 
     private val defaultHttp = HttpServer.Config(
       host = host"0.0.0.0",
--- modules/core/src/main/scala/pillars/HttpServer.scala
+++ modules/core/src/main/scala/pillars/HttpServer.scala
@@ -23,17 +23,22 @@ import pillars.syntax.*
 import sttp.capabilities.StreamMaxLengthExceededException
 import sttp.monad.MonadError
 import sttp.tapir.*
+import sttp.tapir.docs.openapi.OpenAPIDocsOptions
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.server.http4s.Http4sServerOptions
 import sttp.tapir.server.interceptor.exception.ExceptionContext
 import sttp.tapir.server.interceptor.exception.ExceptionHandler
 import sttp.tapir.server.model.ValuedEndpointOutput
+import sttp.tapir.swagger.SwaggerUIOptions
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
 object HttpServer:
     def build[F[_]: Async](
         name: String,
         config: Config,
+        openApi: Config.OpenAPI,
+        infos: AppInfo,
         observability: Observability[F],
         endpoints: List[HttpEndpoint[F]]
     ): Resource[F, Server] =
@@ -56,7 +61,20 @@ object HttpServer:
                 .exceptionHandler(exceptionHandler())
                 .options
 
-        val routes          = Http4sServerInterpreter[F](options).toRoutes(endpoints).orNotFound
+        val openAPIEndpoints = if openApi.enabled then
+            SwaggerInterpreter(
+              swaggerUIOptions = SwaggerUIOptions(
+                pathPrefix = openApi.pathPrefix,
+                yamlName = openApi.yamlName,
+                contextPath = openApi.contextPath,
+                useRelativePaths = openApi.useRelativePaths,
+                showExtensions = openApi.showExtensions
+              )
+            ).fromServerEndpoints(endpoints, name, infos.version)
+        else Nil
+
+        val routes = Http4sServerInterpreter[F](options).toRoutes(endpoints ++ openAPIEndpoints).orNotFound
+
         val app: HttpApp[F] = routes |> logging |> errorHandling |> cors
 
         NettyServerBuilder[F].withoutSsl.withNioTransport
@@ -107,7 +125,17 @@ object HttpServer:
     object Config:
         given Configuration = Configuration.default.withKebabCaseMemberNames.withKebabCaseConstructorNames.withDefaults
 
-        given Codec[Config] = Codec.AsObject.derivedConfigured
+        given Codec[Config.OpenAPI] = Codec.AsObject.derivedConfigured
+        given Codec[Config]         = Codec.AsObject.derivedConfigured
+
+        final case class OpenAPI(
+            enabled: Boolean = false,
+            pathPrefix: List[String] = List("docs"),
+            yamlName: String = "pillars.yaml",
+            contextPath: List[String] = Nil,
+            useRelativePaths: Boolean = true,
+            showExtensions: Boolean = false
+        )
 
     end Config
 end HttpServer
