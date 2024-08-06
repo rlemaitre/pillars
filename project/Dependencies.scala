ada61890293ab9e7692832dd781f652337e086b4
Scala Steward <scala_steward@virtuslab.com>
2024-08-06 01:48:34 +0000

    chore: Update tapir-json-circe from 1.10.15 to 1.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
 
     private val tapir = Seq(
       "com.softwaremill.sttp.tapir"   %% "tapir-http4s-server"         % "1.11.0",
-      "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"            % "1.10.15",
+      "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"            % "1.11.0",
       "com.softwaremill.sttp.tapir"   %% "tapir-opentelemetry-metrics" % "1.10.15",
       "com.softwaremill.sttp.tapir"   %% "tapir-iron"                  % "1.11.0",
       "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs"          % "1.10.15",
