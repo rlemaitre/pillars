0089548c45a398a43c0985196f163380acbeaa34
Scala Steward <scala_steward@virtuslab.com>
2024-09-10 16:34:29 +0000

    chore: Update http4s-netty-client from 0.5.18 to 0.5.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     )
 
     val http4sClient: Seq[ModuleID] = Seq(
-      "org.http4s"                  %% "http4s-netty-client" % "0.5.18",
+      "org.http4s"                  %% "http4s-netty-client" % "0.5.19",
       "com.softwaremill.sttp.tapir" %% "tapir-http4s-client" % "1.11.2",
       "com.alejandrohdezma"         %% "http4s-munit"        % "1.1.0" % Test
     )
