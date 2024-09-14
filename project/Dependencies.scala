c61031837a4ccbf6178156b64c9ecf3505b1172d
Scala Steward <scala_steward@virtuslab.com>
2024-09-14 17:32:35 +0000

    chore: Update tapir-iron from 1.11.2 to 1.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
       "com.softwaremill.sttp.tapir"   %% "tapir-http4s-server"         % "1.11.3",
       "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"            % "1.11.2",
       "com.softwaremill.sttp.tapir"   %% "tapir-opentelemetry-metrics" % "1.11.2",
-      "com.softwaremill.sttp.tapir"   %% "tapir-iron"                  % "1.11.2",
+      "com.softwaremill.sttp.tapir"   %% "tapir-iron"                  % "1.11.3",
       "com.softwaremill.sttp.tapir"   %% "tapir-openapi-docs"          % "1.11.2",
       "com.softwaremill.sttp.apispec" %% "openapi-circe-yaml"          % "0.11.3",
       "com.softwaremill.sttp.tapir"   %% "tapir-swagger-ui-bundle"     % "1.11.2",
