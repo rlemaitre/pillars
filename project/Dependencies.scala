a1d39546afa42615ca2ae8a949b45626e120338a
Scala Steward <scala_steward@virtuslab.com>
2024-10-28 18:47:44 +0000

    chore: Update http4s-dsl from 0.23.28 to 0.23.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
 
     val http4s: Seq[ModuleID] = Seq(
       "org.http4s" %% "http4s-core"  % "0.23.29",
-      "org.http4s" %% "http4s-dsl"   % "0.23.28",
+      "org.http4s" %% "http4s-dsl"   % "0.23.29",
       "org.http4s" %% "http4s-circe" % "0.23.29"
     )
 
