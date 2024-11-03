755c34f0bc8986419d8b261d1e9d8f04368e265d
Scala Steward <scala_steward@virtuslab.com>
2024-11-03 18:00:34 +0000

    chore: Update otel4s-sdk, otel4s-sdk-exporter from 0.8.1 to 0.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -81,8 +81,8 @@ object Dependencies {
     )
 
     val observability: Seq[ModuleID] = Seq(
-      "org.typelevel" %% "otel4s-sdk"          % "0.8.1",
-      "org.typelevel" %% "otel4s-sdk-exporter" % "0.8.1"
+      "org.typelevel" %% "otel4s-sdk"          % "0.11.0",
+      "org.typelevel" %% "otel4s-sdk-exporter" % "0.11.0"
     )
 
     val skunk: Seq[ModuleID] = Seq(
