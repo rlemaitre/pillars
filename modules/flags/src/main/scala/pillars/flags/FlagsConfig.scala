f0bb12a2d383a4f2c65e446895cd094d41a881aa
Raphaël Lemaitre <rlemaitre@users.noreply.github.com>
2024-12-03 14:14:30 +0100

    chore: Change license from APL-2.0 to EPL-2.0 (#190)
    Signed-off-by: Raphaël Lemaitre <rlemaitre@users.noreply.github.com>
    Co-authored-by: Scala Steward <scala_steward@virtuslab.com>

--- modules/flags/src/main/scala/pillars/flags/FlagsConfig.scala
+++ modules/flags/src/main/scala/pillars/flags/FlagsConfig.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2024-2024 by Raphaël Lemaitre and Contributors
+// This software is licensed under the Eclipse Public License v2.0 (EPL-2.0).
+// For more information see LICENSE or https://opensource.org/license/epl-2-0
+
 package pillars.flags
 
 import io.circe.Codec
