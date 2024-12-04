0e9cfbd457f55b7d101a7a0b95f237bd015ea96c
Scala Steward <scala_steward@virtuslab.com>
2024-12-04 00:45:32 +0000

    chore: Update tapir-http4s-client from 1.11.9 to 1.11.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
 
     val http4sClient: Seq[ModuleID] = Seq(
       "org.http4s"                  %% "http4s-netty-client" % "0.5.21",
-      "com.softwaremill.sttp.tapir" %% "tapir-http4s-client" % "1.11.9",
+      "com.softwaremill.sttp.tapir" %% "tapir-http4s-client" % "1.11.10",
       "com.alejandrohdezma"         %% "http4s-munit"        % "1.1.0" % Test
     )
     val http4sServer: Seq[ModuleID] = Seq(
